4e2d3131e629306f74c233bcf75141552b398191
C18324463 <c18324463@mytudublin.ie>
2021-12-10 17:25:06 +0000

    box instantiation

---
+++ Assignment/Assets/levelgenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelgenerator : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector3(1, 0.5f, 8f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

052d4272f0b0efae8c6d7cb34f0b70fef0da6a29
C18324463 <c18324463@mytudublin.ie>
2021-12-13 20:05:35 +0000

    Scoreboard added

--- Assignment/Assets/Scripts/CarController.cs
+++ Assignment/Assets/Scripts/CarController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
     public float speed = 3f;
     public float rotSpeed = 30f;
+    public int score = -1;
 
     public GameObject Car;
+    public GameObject Cube;
+    public GameObject Cube1;
+    public GameObject Cube2;
+    public Text word;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +25,7 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        word.text = "score: " + score;
         float c = Input.GetAxis("Vertical") * speed;
         float r = Input.GetAxis("Horizontal") * speed;
         transform.Translate(new Vector3(r, 0, c) * Time.deltaTime);
@@ -25,10 +33,16 @@ public class CarController : MonoBehaviour
         if (Car.transform.position.y < -1) {
             Application.LoadLevel("RoadScene");
         }
+
+        if (Car.transform.position.z > Cube.transform.position.z || Car.transform.position.z > Cube1.transform.position.z || Car.transform.position.z > Cube2.transform.position.z) {
+            score = score + 1;
+            Debug.Log(score);
+        }
+
     }
 
     void OnCollisionEnter (Collision collision) {
-        if (collision.gameObject.name == "Cube" || collision.gameObject.name == "Cube (1)" || collision.gameObject.name == "Cube (2)") {
+        if (collision.gameObject.name == "Cube" || collision.gameObject.name == "Cube1" || collision.gameObject.name == "Cube2") {
             Application.LoadLevel("RoadScene");
         }
     }

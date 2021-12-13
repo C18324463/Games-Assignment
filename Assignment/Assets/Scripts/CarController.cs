ce0b519ba74b892ed8ccd0ae6c9fa404f2f79fec
C18324463 <c18324463@mytudublin.ie>
2021-12-13 21:16:53 +0000

    Fixed size of highscore

--- Assignment/Assets/Scripts/CarController.cs
+++ Assignment/Assets/Scripts/CarController.cs
@@ -8,42 +8,52 @@ public class CarController : MonoBehaviour
     public float speed = 3f;
     public float rotSpeed = 30f;
     public int score = -1;
+    public int highscore = 0;
 
     public GameObject Car;
     public GameObject Cube;
     public GameObject Cube1;
     public GameObject Cube2;
-    public Text word;
+    public Text word1;
+    public Text word2;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.GetInt("High Score");
     }
 
     // Update is called once per frame
     void Update()
     {
-        word.text = "score: " + score;
-        float c = Input.GetAxis("Vertical") * speed;
-        float r = Input.GetAxis("Horizontal") * speed;
-        transform.Translate(new Vector3(r, 0, c) * Time.deltaTime);
+        word1.text = "SCORE:" + score;
+        word2.text = "HIGHSCORE:" + highscore;
+        float v = Input.GetAxis("Vertical") * speed;
+        float h = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(new Vector3(h, 0, v) * Time.deltaTime);
 
         if (Car.transform.position.y < -1) {
             Application.LoadLevel("RoadScene");
+            PlayerPrefs.SetInt("High Score", highscore);
         }
 
         if (Car.transform.position.z > Cube.transform.position.z || Car.transform.position.z > Cube1.transform.position.z || Car.transform.position.z > Cube2.transform.position.z) {
             score = score + 1;
             Debug.Log(score);
+            if (highscore < score) {
+                highscore = score;
+                Debug.Log(highscore + "top");
+            }
         }
 
     }
 
     void OnCollisionEnter (Collision collision) {
         if (collision.gameObject.name == "Cube" || collision.gameObject.name == "Cube1" || collision.gameObject.name == "Cube2") {
+            highscore = score;
             Application.LoadLevel("RoadScene");
+            //highscore = score;
         }
     }
 }

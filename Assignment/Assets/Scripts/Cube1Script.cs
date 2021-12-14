b0a2479fdfe741b2dbafb0a07674d81d6d696226
C18324463 <c18324463@mytudublin.ie>
2021-12-14 21:50:14 +0000

    Update Cube1Script.cs
    Added comments

--- Assignment/Assets/Scripts/Cube1Script.cs
+++ Assignment/Assets/Scripts/Cube1Script.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Class
 public class Cube1Script : MonoBehaviour
 {
+    //Game Objects
     public GameObject Cube;
     public GameObject Car;
     
+    //Variables
     int i = 1;
 
     // Start is called before the first frame update
@@ -18,14 +21,21 @@ public class Cube1Script : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        //If cars z position is greater than cubes z position
         if (Car.transform.position.z > Cube.transform.position.z) {
+            //Set the max and min for the cubes spawning location
             float min = -2.5f;
             float max = -3.5f;
+            //Puts them into a random x variable
             float randomx = Random.Range(min, max);
+            //Adds 5 and i to the min and max to keep to moving
             min = 5 + i;
             max = 5 + i + 5;
+            //Puts them into a random z variable
             float randomz = Random.Range(min, max);
+            //Cubes position randomly spawns within the area
             Cube.transform.position = new Vector3(randomx, 0.5f, (randomz + 5));
+            //Adds 5 to i
             i = i + 5;
         }
     }
